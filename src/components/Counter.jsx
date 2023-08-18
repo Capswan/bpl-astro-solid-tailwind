51d678fd9a2bbbd341305f053592a701dc711519
alechp <me@alechp.com>
2023-08-18 16:19:56 -0700

    astro, solidjs and tailwind portions working together

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+import { createSignal, onCleanup } from "solid-js";
+
+export const Counter = () => {
+  const [count, setCount] = createSignal(0);
+
+  const interval = setInterval(() => setCount((c) => c + 1), 1000);
+
+  onCleanup(() => clearInterval(interval));
+
+  return <div>Count value is {count()}</div>;
+};
