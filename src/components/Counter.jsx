ababf769ac899947f6a9d1bd89f25ac9f4862d7f
alechp <me@alechp.com>
2023-08-18 21:03:13 -0700

    Finished integrating Astro, Solid, Flowbite and Tailwind

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,13 @@
 import { createSignal, onCleanup } from "solid-js";
+import { render } from "solid-js/web";
 
 export const Counter = () => {
-  const [count, setCount] = createSignal(0);
-
-  const interval = setInterval(() => setCount((c) => c + 1), 1000);
-
-  onCleanup(() => clearInterval(interval));
-
-  return <div>Count value is {count()}</div>;
+	const [count, setCount] = createSignal(0);
+	const interval = setInterval(
+		() => setCount(c => c + 1),
+		1000
+	);
+	onCleanup(() => clearInterval(interval));
+	return <div>Count value is {count()}</div>;
 };
+
