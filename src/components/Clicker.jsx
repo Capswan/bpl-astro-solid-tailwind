94d83aab8c6d27fa1f84edf1b7f1d3a67732501d
alechp <me@alechp.com>
2023-08-20 16:48:52 -0700

    jsdoc

--- src/components/Clicker.jsx
+++ src/components/Clicker.jsx
@@ -1,7 +1,8 @@
 import { createSignal } from "solid-js";
 import { Counter } from "./Counter.jsx"
 
-export function Clicker() {
+
+export function Clicker(foo, bar) {
 	const [count, setCount] = createSignal(0);
 	const add = () => setCount(count() + 1);
 	const subtract = () => setCount(count() - 1);
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,15 @@
 import { createSignal, onCleanup } from "solid-js";
 import { render } from "solid-js/web";
 
-export const Counter = () => {
-	const [count, setCount] = createSignal(0);
-	const interval = setInterval(
-		() => setCount(c => c + 1),
-		1000
-	);
-	onCleanup(() => clearInterval(interval));
-	return <div>Count value is {count()}</div>;
-};
+/**
+ * Initializes a counter component
+ *
+ * @param {number} startingIndex - The starting index of the counter
+ */
+export function Counter(startingIndex) {
+  const [count, setCount] = createSignal(startingIndex);
+  const interval = setInterval(() => setCount((c) => c + 1), 1000);
+  onCleanup(() => clearInterval(interval));
+  return <div>Count value is {count()}</div>;
+}
 
