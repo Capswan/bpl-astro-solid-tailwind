c90bc5337ef341f72c520c30b09bc13d76fe02da
alechp <me@alechp.com>
2023-08-20 18:46:36 -0700

    boilerplate was starting to bloat; removed excess

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,9 @@
-import { createSignal, onCleanup } from "solid-js";
+import { createSignal, onCleanup, splitProps } from "solid-js";
 import { render } from "solid-js/web";
 
-/**
- * Initializes a counter component
- *
- * @param {number} startingIndex - The starting index of the counter
- */
-export function Counter(startingIndex) {
-  const [count, setCount] = createSignal(startingIndex);
+export function Counter(props) {
+  const [count, setCount] = createSignal(0);
   const interval = setInterval(() => setCount((c) => c + 1), 1000);
   onCleanup(() => clearInterval(interval));
-  return <div>Count value is {count()}</div>;
+  return <div>Count value is {count()} </div>;
 }
--- src/components/Flowbitten.jsx
+++ src/components/Flowbitten.jsx
@@ -1280,7 +1280,9 @@ export function Flowbitten() {
         </div>
         <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
         <div class="grid grid-cols-2 gap-4 mb-4">
-          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"><Counter /></div>
+          <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72">
+            <Counter />
+          </div>
           <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
           <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
           <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72"></div>
